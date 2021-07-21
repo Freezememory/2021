9e4f908e7364f7d682238f2ce47ed25795eaf8ae
Freezememory <1152589335@qq.com>
2021-07-21 16:44:09 +0800

    2021设计模式联系项目上传码云

---
+++ src/main/java/com/wanglj/exercise/ExerciseSpringBootApplication.java
@@ -0,0 +1,17 @@
+package com.wanglj.exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Wanglj
+ * @date 2021/7/21 15:47
+ */
+@SpringBootApplication
+public class ExerciseSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExerciseSpringBootApplication.class, args);
+
+    }
+}
---
+++ src/main/java/com/wanglj/exercise/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.wanglj.exercise.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Wanglj
+ * @date 2021/7/21 16:11
+ */
+@RestController
+public class TestController {
+
+
+    @RequestMapping(value = "/test")
+    public String test() {
+        String a = "hello  springBoot!!!!";
+        System.out.println("springboot 启动！");
+        return a;
+    }
+}
