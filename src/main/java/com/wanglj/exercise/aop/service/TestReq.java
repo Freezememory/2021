cddad3fee6a6490ec6daa0139b66438848fe2c67
Freezememory <1152589335@qq.com>
2021-09-07 10:58:29 +0800

    实体类build测试

--- src/main/java/com/wanglj/exercise/aop/service/TestReq.java
+++ src/main/java/com/wanglj/exercise/aop/service/TestReq.java
@@ -1,6 +1,9 @@
 package com.wanglj.exercise.aop.service;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
@@ -9,19 +12,24 @@ import java.time.LocalDateTime;
  * @date 2021/9/4 14:44
  */
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class TestReq {
 
     private LocalDateTime dateTime;
 
     private String name;
 
+
+/*
     public String getName() {
         return "有点意思";
-    }
+    }*/
 
     public static void main(String[] args) {
-        TestReq req = new TestReq();
-        req.setDateTime(LocalDateTime.now());
+        TestReq req = TestReq.builder().name("abc").dateTime(LocalDateTime.now()).build();
+        //req.setDateTime(LocalDateTime.now());
         System.out.println(req.toString());
     }
 }
