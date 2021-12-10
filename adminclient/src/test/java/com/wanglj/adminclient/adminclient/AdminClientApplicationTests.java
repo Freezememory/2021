602abcfc94dc075b64fc1192980bd2962567b82b
Freezememory <1152589335@qq.com>
2021-12-10 18:10:10 +0800

    spring-admin监控

---
+++ adminclient/src/test/java/com/wanglj/adminclient/adminclient/AdminClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanglj.adminclient.adminclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
